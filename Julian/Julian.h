89d5bb4fd1a1bcf2787818af2ed1b9d491a560c9
Aaron Stanek <amr.stanek@gmail.com>
2021-10-07 16:30:51 -0700

    added JulianEncode and JulianDecode to Julian.h

--- Julian/Julian.h
+++ Julian/Julian.h
@@ -4,4 +4,8 @@
 #include "../common/CalendarCache.h"
 #include "../common/YMD.h"
 
+int JulianEncode(const struct CalendarCache* const restrict cache, struct YMD* const restrict output, const long udn);
+
+int JulianDecode(const struct CalendarCache* const restrict cache, long* output, const struct YMD* const restrict ymd);
+
 #endif
