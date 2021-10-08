c24379c3602f20368a6cb3f52dc82fee6285defa
Aaron Stanek <amr.stanek@gmail.com>
2021-10-07 17:03:17 -0700

    small datatypes in YMD

--- common/YMD.h
+++ common/YMD.h
@@ -1,10 +1,12 @@
 #ifndef __YMD_H__
 #define __YMD_H__
 
+#include <stdint.h>
+
 struct YMD {
-    long year;
-    unsigned char month;
-    unsigned char day;
+    int_least32_t year;
+    uint_least8_t month;
+    uint_least8_t day;
 };
 
 #endif
