bb06d5611c06535eacdb3e616c6b6bf7c5b0c790
Aaron Stanek <amr.stanek@gmail.com>
2021-10-07 17:05:41 -0700

    small datatypes in JulianGregorianMonthCache

--- JulianGregorianMonthCache/JulianGregorianMonthCache.h
+++ JulianGregorianMonthCache/JulianGregorianMonthCache.h
@@ -1,11 +1,13 @@
 #ifndef __JULIANGREGORIANMONTHCACHE_H__
 #define __JULIANGREGORIANMONTHCACHE_H__
 
+#include <stdint.h>
+
 struct JulianGregorianMonthCache {
-    unsigned char length[12];
-    unsigned char lengthLeap[12];
-    unsigned short total[12];
-    unsigned short totalLeap[12];
+    uint_least8_t length[12];
+    uint_least8_t lengthLeap[12];
+    uint_least16_t total[12];
+    uint_least16_t totalLeap[12];
 };
 
 void JulianGregorianMonthCache__constructor(struct JulianGregorianMonthCache* const restrict cache);
