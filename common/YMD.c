32f30c1aa7e030d1e6c047f82a5539588aab29a9
Aaron Stanek <amr.stanek@gmail.com>
2021-10-25 10:43:14 -0700

    wrote YMD__toString

---
+++ common/YMD.c
@@ -0,0 +1,49 @@
+#include "YMD.h"
+
+#include "CommonFunctions.h"
+#include "ErrorCodes.h"
+
+#include <stdlib.h>
+
+UniversalCalendarErrorCode YMD__toString(struct YMD* const restrict ymd, char** const restrict output, const char* const restrict prefix) {
+    struct DynamicCharArray string;
+    UniversalCalendarErrorCode e;
+    e = DynamicCharArray__constructor(&string);
+    if (e) {
+        // if the constructor fails, then no memory is allocated
+        // and there is no need to destruct
+        return e;
+    };
+    // if the constructor succeeds, but there is a failure later
+    // we will need to destruct
+    e = DynamicCharArray__pushArray(&string,prefix);
+    if (e) {
+        free(string.data);
+        return e;
+    }
+    e = DynamicCharArray__push(&string,'(');
+    if (e) {
+        free(string.data);
+        return e;
+    }
+    e = DynamicCharArray__pushIntegerChar(&string,ymd->year,',');
+    if (e) {
+        free(string.data);
+        return e;
+    }
+    e = DynamicCharArray__pushIntegerChar(&string,ymd->month,',');
+    if (e) {
+        free(string.data);
+        return e;
+    }
+    e = DynamicCharArray__pushIntegerChar(&string,ymd->day,')');
+    if (e) {
+        free(string.data);
+        return e;
+    }
+    // the string should be fully constructed at this point
+    // we can return it, the responsibility to destruct
+    // is passed to the caller
+    *output = string.data;
+    return NO_ERROR;
+}
--- common/YMD.h
+++ common/YMD.h
@@ -1,6 +1,8 @@
 #ifndef __YMD_H__
 #define __YMD_H__
 
+#include "ErrorType.h"
+
 #include <stdint.h>
 
 struct YMD {
@@ -8,5 +10,7 @@ struct YMD {
     uint_least8_t month;
     uint_least8_t day;
 };
+
+UniversalCalendarErrorCode YMD__toString(struct YMD* const restrict ymd, char** const restrict output, const char* const restrict prefix);
 
 #endif
